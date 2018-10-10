15f19be89617aca7bc7759ee175b8a9eda3d6d9b
Thorgeir <thorgeir17@ru.is>
2018-10-10 15:49:36 +0000

    Add for two numbers

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,13 @@ function add (number) {
   if(number == "") {
     return 0;
   }
-  return parseInt(number);
+  if(number.includes(",")) {
+    var numberArray = number.split(",");
+    return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+  }
+  else {
+    return parseInt(number);
+  }
 }
 
 module.exports = add;
