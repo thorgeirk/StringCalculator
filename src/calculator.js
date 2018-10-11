6a5c5a4a4f140c8fa64d49b2518c0c6d8dce592e
Thorgeir <thorgeir17@ru.is>
2018-10-11 17:21:13 +0000

    Displays a dilimiter

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,15 @@ function add (numbers) {
   if(numbers == "") {
     return 0;
   }
-  if(numbers.includes(",") | ("\n") ) {
+  if(numbers[0] == "/" && (numbers[1] == "/")) {
+    var delimiter = numbers.substring(
+      numbers.lastIndexOf("/") + 1,
+      numbers.lastIndexOf("\n")
+    );
+    return delimiter;
+  }
+
+  else if(numbers.includes(",") | ("\n")) {
     var numbersArray = numbers.split(/[\n,]+/);
 
     var negativeArray = [];
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -30,3 +30,7 @@ test("should throw an error message along with all negative numbers in the set",
 it("should ignore numbers bigger than 1000", () => {
   expect(add("1001,2")).toBe(2);
 });
+
+it("should display a delimiter", () => {
+  expect(add("//;\n1;2")).toBe(";");
+});
